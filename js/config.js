81e6e0a91ff7375ffa72fc4e7b788cfa76a00f94
arrobasdigital <arrobasdigital@gmail.com>
2026-09-23 10:16:44 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 window.CPAP_CONFIG = {
-  whatsappNumber: "5531983653829",
+  whatsappNumber: "5531983818037",
   defaultMessage: "Olá, vim do Google e gostaria de falar com a CPAP Minas.",
   googleAdsConversionLabel: "", // opcional: preencha se for disparar conversão direta via gtag
   includeUtmInWhatsapp: false
